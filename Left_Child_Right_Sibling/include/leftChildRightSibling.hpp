76aa5f66e8a956ef339ba578a7acf8c041092351
Oziel Alves <ozielalves7@gmail.com>
2018-10-19 20:33:52 -0300

    Tree update

--- Left_Child_Right_Sibling/include/leftChildRightSibling.hpp
+++ Left_Child_Right_Sibling/include/leftChildRightSibling.hpp
@@ -115,47 +115,6 @@ class Tree{
             this->root->value = value;
         }
 
-   //      Node<T> *pushSibling(Node<T> *n, T value){
-   //          if(n == NULL) return NULL;
-            
-			// while(n->sibling)
-   //              n = n->sibling;
-
-   //          return (n->sibling = new Node<T>(value));
-   //      }
-
-   //      Node<T> *pushChild(T value, Node<T> *n = NULL){
-   //          if(n == NULL){
-   //              if(this->root == NULL) return NULL;
-
-   //              n = root;
-   //          }
-
-   //          // Se o nó já tiver um filho então adicionamos um irmão
-   //          if(n->child)
-   //              return pushSibling(n->child, value);
-
-   //          return (n->child = new Node<T>(value));
-   //      }
-
-   //      void print(Node<T> *n = NULL){
-   //          if(n == NULL){
-   //              if(this->root == NULL) return;
-
-   //              n = root;
-   //          }
-
-   //          while(n){
-   //              std::cout << " " << n->value;
-                
-   //              if(n->child)
-   //                  print(n->child);
-
-   //              n = n->sibling;
-   //          }
-
-   //      }
-
         /**
          * @brief Calculate the tree height.
          * @param *n The tree.
@@ -185,58 +144,25 @@ class Tree{
             return h+1; 
         }
 
-        
-
-		// Node<T> * searchSibling_node(Node<T> *sibling, T value) {
-
-		// 	if (sibling->value == value) return sibling;
-		// 	}else if (sibling->child != NULL and sibling->sibling == NULL){ /*child:1 sibling:0*/
-		// 	    return searchChild_node(sibling->child, value);
-		// 	}else if (sibling->child == NULL and sibling->sibling != NULL){ /*child:0 sibling:1*/
-		// 	    return searchSibling_node(sibling->sibling, value);
-		// 	}else if (sibling->child != NULL and sibling->sibling != NULL){ /*child:1 sibling:1*/
-			
-		// 		// 1 ) Search starts for the Siblings
-		// 		auto siblings = searchSibling_node(sibling->sibling, value);
-
-		// 		if (siblings != NULL)
-		// 		{
-		// 			return siblings;
-		// 		}
-
-		// 		// 2 ) Didn't find on sibling, search on Childs
-		// 		return searchChild_node(sibling->child, value);
-		// 	}
-
-
-  //       	return NULL;   // Not founded
-
-		// }
-
-		// Node<T> * search_node(T value) {
-		//     return this->searchChild_node(this->root, value);
-		// }
-
         /**
          * @brief Removes an element from the tree.
          * @param value The element to be removed.
          */
-		void remove(T value){
-			auto *node = this->busca(value);
+		    void remove(T value){
+          auto *node = this->busca(value);
 
-			if(node == NULL){
-				std::cout << "\nValor nao esta na Arvore\n";
-			} else if(node == this->root) {
-				this->root = NULL;
-			} else {
-				if(node->sibling == NULL) {
-					//node = NULL; // Supostamente deveria funcionar mas ainda é impresso
-					*node = NULL; // Funciona, porém atribui "0" a node->value
-				} else {
-					*node = *node->sibling;
-				}
-			}
-		}
+          if(node == NULL){
+            std::cout << "\nValor nao esta na Arvore\n";
+          } else if(node == this->root) {
+            this->root = NULL;
+          } else {
+            if(node->sibling == NULL) {
+              *node = NULL;
+				    } else {
+            *node = *node->sibling;
+				    }
+          }
+		    }
 };
 
 #endif
